d92e371807ae218d734aa5bf53a8db8298bd2aa7
jj1guj <jj1guj@jj1guj.net>
2026-08-22 18:34:30 +0900

    jsonldのエラーでdelayになったときにエラー内容をログに出力するように

--- packages/backend/src/queue/QueueProcessorService.ts
+++ packages/backend/src/queue/QueueProcessorService.ts
@@ -171,10 +171,13 @@ export class QueueProcessorService implements OnApplicationShutdown {
 				return `${e.name}: ${e.message}`;
 			}
 
+			const details = 'details' in e ? (e as Error & { details?: unknown }).details : undefined;
+
 			return {
 				stack: e.stack,
 				message: e.message,
 				name: e.name,
+				...(details !== undefined ? { details } : {}),
 			};
 		}
 
